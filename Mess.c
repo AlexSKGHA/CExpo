c23fdb26a20fc190d289e4f5abdccf6bef79eda9
AlexSKGHA <41026477+AlexSKGHA@users.noreply.github.com>
2024-03-29 00:40:29 +0400

    Update Mess.c

--- Mess.c
+++ Mess.c
@@ -55,7 +55,38 @@ int isPalindrome(char string[]) {
   return isSame;
 }
 
+int compareStrings(char *string1, char *string2) {
+
+  size_t length = strlen(string1);
+  if (length != strlen(string2)) return 0;
+
+  for (size_t i = 0; i < length; i++) {
+    if (string1[i] != string2[i]) return 0;
+  }
+
+  return 1;
+}
+
+void printFlag() {
+  size_t starsPerLine = 6;
+  size_t starLines = 5;
+  size_t stripLength = 40;
+  for (int i = 0; i < 13; i++) {
+    for (int s = 0; s < stripLength; s++) {
+      if (s < starsPerLine * 2 && i < starLines) {
+        if (i % 2) {
+          printf("%c", s % 2? '*' : ' ');
+        } else {
+          printf("%c", s % 2? ' ': '*');
+        }
+      } else {
+        printf("=");
+      }
+    }
+    printf("\n");
+  }
+}
 
 int main(void) {
-
+  printFlag();
 }
